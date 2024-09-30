75a51c1f0671f1cab4143911f989e87f32bd9bfd
MuhammadFadly090 <161425365+MuhammadFadly090@users.noreply.github.com>
2024-09-30 19:40:33 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import "./App.css";
 import Hello from "./Hello";
 import Produk from "./Produk";
+import Registrasi from "./Registrasi";
+
+
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
             <Hello 
-                nama="Muhammad Fadly Ukhrowi Akhfir" 
+                nama="Muhammad fadly ukhrowi akhfir" 
                 pesan="Semoga hari ini lebih baik dari sebelumnya"/>
                 <Produk />
+                <Registrasi />
             </header>
         </div>
     );
